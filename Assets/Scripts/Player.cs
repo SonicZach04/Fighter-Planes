e0e58dbfd4338c5ea8f49811022c6cb34ac7d7a0
SpartOtOnYas <thehuntman0135@gmail.com>
2024-11-03 18:59:21 -0500

    Updated Player Script for wrap around and strictly horizontal movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // its access level: public or private
-    // its type: int (5, 8, 36, etc.), float (2.5f, 3.7f, etc.)
-    // its name: speed, playerSpeed --- Speed, PlayerSpeed
-    // optional: give it an initial value
     private float speed;
     private int lives = 3;
     private int score = 0;
     private float horizontalInput;
-    private float verticalInput;
 
     public GameObject bullet;
+
+    private float screenHalfHeight;
+    private float screenHalfWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 5f;
+
+        // Check if the camera exists
+        if (Camera.main != null)
+        {
+            // Calculate the half height and half width of the screen in world units
+            screenHalfHeight = Camera.main.orthographicSize; // Height of the camera in orthographic mode
+            screenHalfWidth = Camera.main.aspect * screenHalfHeight; // Width based on the aspect ratio
+        }
+        else
+        {
+            Debug.LogError("Main Camera not found. Please ensure there is a camera tagged as 'MainCamera' in the scene.");
+        }
     }
 
     // Update is called once per frame
@@ -32,32 +42,33 @@ public class Player : MonoBehaviour
     void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
 
-        // if (condition) { //do this }
-        // else if (other condition { //do that }
-        // else { //do this final }
-        if (transform.position.x > 11.5f || transform.position.x <= -11.5f)
+        // Move the player
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * Time.deltaTime * speed);
+
+        // Restrict the player's vertical position to the bottom half of the screen
+        Vector3 newPosition = transform.position;
+        newPosition.y = Mathf.Clamp(newPosition.y, -screenHalfHeight - 1f, -0.5f); // Limit to slightly lower than the bottom half
+        transform.position = newPosition;
+
+        // Wrap around the screen horizontally
+        if (transform.position.x > screenHalfWidth)
         {
-            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+            transform.position = new Vector3(-screenHalfWidth, transform.position.y, 0);
         }
-
-        if (transform.position.y > 8.5f || transform.position.y <= -8.5f)
+        else if (transform.position.x < -screenHalfWidth)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
+            transform.position = new Vector3(screenHalfWidth, transform.position.y, 0);
         }
     }
 
     void Shooting()
     {
-        //if I press SPACE
-        //Create a bullet
+        // If I press SPACE, create a bullet
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Create a bullet
+            // Create a bullet
             Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
     }
-
 }
