e93af2c805fa4bdfbab981dfa9f14621ccca58fb
SonicZach04 <soniczachary04@gmail.com>
2024-11-03 17:41:42 -0800

    Finished Enemy2 scripts
    Made new enemy with new behavior and a script that has it spawn randomly

--- Assets/Scripts/Enemy2.cs
+++ Assets/Scripts/Enemy2.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
+    private int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class Enemy2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(5*direction,-1,0) * Time.deltaTime * 3f);
+
+        if (transform.position.x >= 11.5f || transform.position.x <= -11.5f)
+        {
+            direction *= -1;
+        }
     }
 }
